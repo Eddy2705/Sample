a23b3b9bbb1a90eb96d37def72a106a94dde4654
Edwin <Edwin@SONY-PC>
2020-03-13 16:24:11 +0530

    Property

---
+++ src/main/java/New_Java/Selenium/Learn_Properties.java
@@ -0,0 +1,32 @@
+package New_Java.Selenium;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Learn_Properties {
+
+	public static void main(String[] args) throws IOException {
+	
+		Properties abc = new Properties();
+		
+		FileInputStream input = new FileInputStream("E:\\Maven_New\\Selenium\\src\\main\\java\\New_Java\\Selenium\\IP_file");
+		
+		abc.load(input);
+		String value = abc.getProperty("Browser");
+		
+		System.out.println(value);
+		
+		FileOutputStream OP = new FileOutputStream("E:\\Maven_New\\Selenium\\src\\main\\java\\New_Java\\Selenium\\OP_file");
+		
+		abc.setProperty("Cat", "Kill");
+		abc.setProperty("Boy", "Girl");
+		abc.store(OP, "nan than");
+		
+		
+
+	}
+
+
+}
